2cd9b720c4a29cebb8135c7e84b05b724400d15a
Bau <mauareju@gmail.com>
2025-11-21 06:30:22 +0000

    Py Program to sum received float numbers until 0.0 is received as input using a while loop

---
+++ Python/18.homework7.loop.floatsum.while.py
@@ -0,0 +1,10 @@
+usri=-1.0
+usrsum=0.0
+pren=0.0
+
+while pren!=usri:
+    usri=float(input("Ingresa un numero float:\n"))
+    usrsum+=usri
+
+print("La suma de los numeros ingresados es:")
+print(usrsum)
