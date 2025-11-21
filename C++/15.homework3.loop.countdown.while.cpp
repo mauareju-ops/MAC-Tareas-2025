658df6f7ca7a87fd4e71db56bd9d74c218c595b2
Bau <mauareju@gmail.com>
2025-11-21 03:25:37 +0000

    c++ program with while loop to countdown from a given number

---
+++ C++/15.homework3.loop.countdown.while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int usri;
+	int i=0;
+	cout<<"Ingresa el n numero del que inicia la cuenta regresiva\n";
+	cin>>usri;
+	while(i<=usri){
+		cout<<usri-i<<"\n";
+		i++;
+}
+}
