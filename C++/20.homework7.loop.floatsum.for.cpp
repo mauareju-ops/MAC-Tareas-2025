dc53fffc12bb324eb6c36e396d8b138dbc66eda2
Bau <mauareju@gmail.com>
2025-11-21 06:26:20 +0000

    C++ Program to sum received float numbers until 0.0 is received as input using a while loop

---
+++ C++/20.homework7.loop.floatsum.for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float usri=-1;
+    float usrsum=0;
+    float pren=0.0;
+    while(pren!=usri){
+	cout<<"Ingresa un numero float\n";
+	cin>>usri;
+	usrsum+=usri;
+
+}
+cout<<"La suma de los numeros ingresados es: \n"<<usrsum<<"\n";
+}
