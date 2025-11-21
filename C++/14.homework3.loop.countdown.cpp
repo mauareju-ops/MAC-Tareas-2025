ec3794db86e23c7e534cebc79d561425eb593aab
Bau <mauareju@gmail.com>
2025-11-21 03:21:13 +0000

    c++ program with for loop to countdown from a given number

---
+++ C++/14.homework3.loop.countdown.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int usri;
+	cout<<"Ingresa el n numero del que inicia la cuenta regresiva\n";
+	cin>>usri;
+	for(int i=0;i<=usri;i++){
+		cout<<usri-i<<"\n";
+		
+}
+}
